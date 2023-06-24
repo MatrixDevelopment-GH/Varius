963f89895bdbf1d8770a09cb87839d90ccc0508d
Elbert-Ainstein <elbert4instein@gmail.com>
2023-06-24 23:07:18 +0800

    scratching lotta shits, making it work tonight istg

--- src/commands/chat/bj-command.ts
+++ src/commands/chat/bj-command.ts
@@ -1,8 +1,5 @@
 import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
 import { RateLimiter } from 'discord.js-rate-limiter';
-import { promises as fs } from 'node:fs';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import { cardRank } from '../../constants/card-rank.js';
 import { Language } from '../../models/enum-helpers/index.js';
@@ -11,35 +8,10 @@ import { Lang } from '../../services/index.js';
 import { InteractionUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-async function loadImage(rank: string, suit: string): Promise<Buffer> {
-    const imagePath = join(
-        __dirname,
-        '..',
-        '..',
-        '..',
-        'assets',
-        'images',
-        `${rank}_of_${suit}.png`
-    );
-    console.log(imagePath);
-    try {
-        const imageData = await fs.readFile(imagePath);
-        return imageData;
-    } catch (error) {
-        throw new Error(`Error reading image file '${imagePath}': ${error}`);
-    }
-}
-
 function getRandomCard(): any {
     const rank = cardRank.ranks[Math.floor(Math.random() * cardRank.ranks.length)];
     const suit = cardRank.suits[Math.floor(Math.random() * cardRank.suits.length)];
-    console.log(rank + suit);
-    const card = loadImage(rank, suit);
-    console.log(card);
-    return card;
+    return { rank, suit };
 }
 // TODO: Add buttons for options: Hit/Pass/Double
 export class BjCommand implements Command {
