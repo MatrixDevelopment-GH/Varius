58241859a67074e2e167768d92b1bce727a28d09
ImAFrogOwO <dev.zero06@gmail.com>
2023-06-25 16:37:07 -0500

    pp butt

--- src/commands/chat/index.ts
+++ src/commands/chat/index.ts
@@ -2,3 +2,4 @@ export { HelpCommand } from './help-command.js';
 export { InfoCommand } from './info-command.js';
 export { TestCommand } from './test-command.js';
 export { BjCommand } from './bj-command.js';
+export { PurgeCommand } from './purge-command.js';
