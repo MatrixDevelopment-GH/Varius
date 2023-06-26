cd9ab5664b93aa68624dc03e0c2e5fe4c25cdc51
Elbert-Ainstein <iwu48@yahoo.com>
2023-06-27 05:27:21 +0800

    implementing the portfolio command

--- src/commands/economy/portfolio-command.ts
+++ src/commands/economy/portfolio-command.ts
@@ -4,7 +4,7 @@ import { RateLimiter } from 'discord.js-rate-limiter';
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
 import { Lang } from '../../services/index.js';
-import { InteractionUtils } from '../../utils/index.js';
+import { InteractionUtils, prisma } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
 
 export class PortfolioCommand implements Command {
@@ -16,11 +16,35 @@ export class PortfolioCommand implements Command {
     public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
         let args = {
             option:
-                intr.options.getUser(Lang.getRef('arguments.iq', Language.Default)) ?? intr.user,
+                intr.options.getUser(Lang.getRef('arguments.portfolio', Language.Default)) ?? intr.user,
         };
+        console.log(args.option)
+        const user = await prisma.user.findUnique({
+            where: {
+                user_id: args.option.id
+            },
+            include: {
+                properties: true,
+            }
+        })
+
+        // TODO: get net worth + all other stats
+        function findTotal(): number {
+            let total = 0.0;
+            user.properties.map( (asset) => {
+                total += asset.price
+            });
+            total += user.balance
+            console.log(total);
+            return total;
+        }
 
         let embed = Lang.getEmbed('displayEmbeds.portfolio', data.lang, {
-            
+            MENTIONED_USER: `${args.option}`,
+            NET_WORTH: `${findTotal()}`,
+            CASH: `${user.balance}`,
+            PROPERTY: `${user.properties.map((asset) => asset.name)}`,
+            STOCK: `to be implemented`,
         });
         await InteractionUtils.send(intr, { embeds: [embed] });
     }
