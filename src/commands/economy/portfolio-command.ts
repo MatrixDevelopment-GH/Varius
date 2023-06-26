b1d582d5343f02ce2da263721c5bb04b32cf49eb
Elbert-Ainstein <elbert4instein@gmail.com>
2023-06-26 22:54:54 +0800

    Adding the portfolio command

--- src/commands/economy/portfolio-command.ts
+++ src/commands/economy/portfolio-command.ts
@@ -1,1 +1,20 @@
-export class PortfolioCommand {}
+import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
+import { RateLimiter } from 'discord.js-rate-limiter';
+
+import { Language } from '../../models/enum-helpers/index.js';
+import { EventData } from '../../models/internal-models.js';
+import { Lang } from '../../services/index.js';
+import { InteractionUtils, prisma } from '../../utils/index.js';
+import { Command, CommandDeferType } from '../index.js';
+
+export class PortfolioCommand implements Command {
+    public names = [Lang.getRef('chatCommands.portfolio', Language.Default)];
+    public cooldown = new RateLimiter(1, 5000);
+    public deferType = CommandDeferType.PUBLIC;
+    public requireClientPerms: PermissionsString[] = [];
+
+    public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
+        let embed = Lang.getEmbed('displayEmbeds.portfolio', data.lang);
+        await InteractionUtils.send(intr, { embeds: [embed] });
+    }
+}
