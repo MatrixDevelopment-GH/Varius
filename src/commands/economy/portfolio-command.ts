2cb466edfbf0d349cfb0b595b3081c5f157c4e3d
Elbert-Ainstein <elbert4instein@gmail.com>
2023-06-27 10:53:33 +0800

    Completing the Portfolio Command

--- src/commands/economy/portfolio-command.ts
+++ src/commands/economy/portfolio-command.ts
@@ -4,7 +4,7 @@ import { RateLimiter } from 'discord.js-rate-limiter';
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
 import { Lang } from '../../services/index.js';
-import { InteractionUtils, prisma } from '../../utils/index.js';
+import { InteractionUtils, nwCache, prisma, PrismaUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
 
 export class PortfolioCommand implements Command {
@@ -14,38 +14,97 @@ export class PortfolioCommand implements Command {
     public requireClientPerms: PermissionsString[] = [];
 
     public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
+        let prismaUtils = new PrismaUtils();
+
         let args = {
             option:
-                intr.options.getUser(Lang.getRef('arguments.portfolio', Language.Default)) ?? intr.user,
+                intr.options.getUser(Lang.getRef('arguments.portfolio', Language.Default)) ??
+                intr.user,
         };
-        console.log(args.option)
+
         const user = await prisma.user.findUnique({
             where: {
-                user_id: args.option.id
+                user_id: args.option.id,
             },
             include: {
                 properties: true,
-            }
-        })
+                stocks: true,
+            },
+        });
 
         // TODO: get net worth + all other stats
+        let total = 0.0;
         function findTotal(): number {
-            let total = 0.0;
-            user.properties.map( (asset) => {
-                total += asset.price
+            user.properties.map(asset => {
+                total += asset.price;
             });
-            total += user.balance
+            user.stocks.map(async stock => {
+                let price = await prismaUtils.getStock(stock.ticker);
+                if (price != stock.currentPrice) {
+                    prisma.stock.update({
+                        where: {
+                            id: stock.id,
+                        },
+                        data: {
+                            currentPrice: price,
+                        },
+                    });
+                }
+                total += stock.amount * price;
+            });
+            total += user.balance;
             console.log(total);
             return total;
         }
+
+        function findAssets(): string[] {
+            let assets = [];
+            user.properties.map(asset => {
+                assets.push(asset.name);
+            });
+            console.log(assets);
+            return assets;
+        }
+
+        let fields: any[] = await Promise.all(
+            user.stocks.map(async stock => {
+                let amount = await prismaUtils.getStock(stock.ticker);
+                if (amount != stock.currentPrice) {
+                    prisma.stock.update({
+                        where: {
+                            id: stock.id,
+                        },
+                        data: {
+                            currentPrice: amount,
+                        },
+                    });
+                }
+                return {
+                    name: `${stock.ticker}: ${stock.amount}x `,
+                    value: `**Worth: ** ${(stock.currentPrice * stock.amount).toFixed(2)}$ → ${(
+                        amount * stock.amount
+                    ).toFixed(2)}$`,
+                    inline: true,
+                };
+            })
+        );
+
+        nwCache[user.user_id] = user.balance + total;
+        console.log(nwCache);
 
         let embed = Lang.getEmbed('displayEmbeds.portfolio', data.lang, {
             MENTIONED_USER: `${args.option.username}`,
             NET_WORTH: `${findTotal()}`,
             CASH: `${user.balance}`,
-            PROPERTY: `${(user.properties.map((asset) => asset.name)).toString() == ""  ? user.properties.map((asset) => asset.name) : "This person doesn't own any properties!"}`,
-            STOCK: `to be implemented`,
-        });
+            PROPERTY: `${
+                findAssets().length == 0
+                    ? Lang.getRef('portfolioDescs.no_props', data.lang)
+                    : findAssets().map(asset => asset)
+            }`,
+            STOCKS: `${
+                fields.length == 0 ? Lang.getRef('portfolioDescs.no_stocks', data.lang) : ''
+            }`,
+        }).addFields(...fields);
         await InteractionUtils.send(intr, { embeds: [embed] });
     }
 }
--- src/utils/prisma-utils.ts
+++ src/utils/prisma-utils.ts
@@ -1,4 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+
+export class PrismaUtils {
+    public async getData(url: string): Promise<any> {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return await res.json();
+    }
+
+    public async getStock(ticker: string): Promise<any> {
+        ticker = ticker.toUpperCase();
+        if (cache[ticker] && cache[ticker].lastUpdated - new Date().getTime() < 300000) {
+            return cache[ticker].value;
+        } else {
+            let tick = await this.getData(
+                `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?apiKey=${process.env.API_KEY}`
+            );
+            if (tick.status != 'OK' || tick.resultsCount == 0)
+                cache[ticker] = { lastUpdated: Number.MAX_SAFE_INTEGER, value: 0 };
+            else cache[ticker] = { lastUpdated: new Date().getTime(), value: tick.results[0].c };
+            return cache[ticker].value;
+        }
+    }
+}
 
 export let cache = {};
 export let nwCache = {};
